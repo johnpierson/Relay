fa44e806810b94bf8ac74cdfa338d66a7372339e
john pierson <sixtysecondrevit@gmail.com>
2021-08-16 11:17:26 -0600

    Move methods. Cleanup Command.cs

--- src/Revit/Command.cs
+++ src/Revit/Command.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Input;
@@ -7,7 +6,6 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Windows;
-using Dynamo.Applications;
 using Relay.Utilities;
 using Application = Autodesk.Revit.ApplicationServices.Application;
 using TaskDialog = Autodesk.Revit.UI.TaskDialog;
@@ -92,51 +90,7 @@ namespace Relay
                 return Result.Failed;
             }
 
-            //toggle the graph to automatic. this is required for running Dynamo UI-Les
-            DynamoUtils.SetToAutomatic(dynamoJournal);
-
-            //this is the UI stuff
-            //if (File.Exists(Path.Combine(Globals.ExecutingPath, "Prlx.RelayUI.dll")))
-            //{
-            //    try
-            //    {
-            //        Prlx.RelayUI.UI.SampleWindow sample = Prlx.RelayUI.Utilities.BuildUI(dynamoJournal);
-            //        sample.ShowDialog();
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        string messages = e.Message;
-            //        //suppress the warning something went wrong.
-            //    }
-            //}
-
-            DynamoRevit dynamoRevit = new DynamoRevit();
-
-
-
-            IDictionary<string, string> journalData = new Dictionary<string, string>
-            {
-                {JournalKeys.ShowUiKey, false.ToString()},
-                {JournalKeys.AutomationModeKey, true.ToString()},
-                {JournalKeys.DynPathKey, ""},
-                {JournalKeys.DynPathExecuteKey, true.ToString()},
-                {JournalKeys.ForceManualRunKey, false.ToString()},
-                {JournalKeys.ModelShutDownKey, true.ToString()},
-                {JournalKeys.ModelNodesInfo, false.ToString()},
-            };
-            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData
-            {
-                Application = commandData.Application,
-                JournalData = journalData
-            };
-
-            dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
-
-
-            DynamoRevit.RevitDynamoModel.OpenFileFromPath(dynamoJournal, true);
-            DynamoRevit.RevitDynamoModel.ForceRun();
-
-            return Result.Succeeded;
+            return Methods.DynamoMethods.RunGraph(commandData.Application, dynamoJournal);
         }
     }
     [Transaction(TransactionMode.Manual)]
@@ -156,7 +110,6 @@ namespace Relay
             };
 
             var dialogResult = aboutDialog.Show();
-
 
             return Result.Succeeded;
 
---
+++ src/Revit/Methods/DynamoMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Dynamo.Applications;
+using Relay.Utilities;
+
+namespace Relay.Methods
+{
+    internal static class DynamoMethods
+    {
+        internal static Result RunGraph(UIApplication app, string dynamoJournal)
+        {
+            //toggle the graph to automatic. this is required for running Dynamo UI-Les
+            DynamoUtils.SetToAutomatic(dynamoJournal);
+
+            DynamoRevit dynamoRevit = new DynamoRevit();
+
+
+            IDictionary<string, string> journalData = new Dictionary<string, string>
+            {
+                {JournalKeys.ShowUiKey, false.ToString()},
+                {JournalKeys.AutomationModeKey, true.ToString()},
+                {JournalKeys.DynPathKey, ""},
+                {JournalKeys.DynPathExecuteKey, true.ToString()},
+                {JournalKeys.ForceManualRunKey, false.ToString()},
+                {JournalKeys.ModelShutDownKey, true.ToString()},
+                {JournalKeys.ModelNodesInfo, false.ToString()},
+            };
+            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData
+            {
+                Application = app,
+                JournalData = journalData
+            };
+
+            var result = dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
+
+
+            DynamoRevit.RevitDynamoModel.OpenFileFromPath(dynamoJournal, true);
+            DynamoRevit.RevitDynamoModel.ForceRun();
+
+            return result;
+        }
+    }
+}
