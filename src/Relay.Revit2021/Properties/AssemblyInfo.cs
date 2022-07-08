eb7e78510995b16797f42dfe335322dd65a85b56
john pierson <sixtysecondrevit@gmail.com>
2022-07-08 17:40:50 -0600

    Revit2021 Progress

---
+++ src/Relay.Revit2021/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Relay")]
+[assembly: AssemblyDescription("Relay allows you to add your Dynamo graphs to your Revit rubbon in an okay way.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Parallax Team Inc.")]
+[assembly: AssemblyProduct("Relay Revit C# .NET Add-In")]
+[assembly: AssemblyCopyright("Copyright 2022 (C) John Pierson")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("321044f7-b0b2-4b1c-af18-e71a19252be0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2022.1.0.2")]
+[assembly: AssemblyFileVersion("2022.1.0.2")]
