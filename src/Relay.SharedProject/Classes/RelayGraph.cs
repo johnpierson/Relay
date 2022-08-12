9f087e03aa1bc3f3bf67548ada949f7b1f7f83dd
John <sixtysecondrevit@gmail.com>
2022-08-12 10:01:59 -0600

    Add dependency to RelayGraph Class

--- src/Relay.SharedProject/Classes/RelayGraph.cs
+++ src/Relay.SharedProject/Classes/RelayGraph.cs
@@ -10,5 +10,13 @@ namespace Relay.Classes
         public string GraphDocumentationURL { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
+        public Dependency[] NodeLibraryDependencies { get; set; }
     }
+
+    internal class Dependency
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+    }
+}
 }
