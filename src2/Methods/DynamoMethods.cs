27f3fa89cacc244406705d8e879220cd0c0dca73
John <sixtysecondrevit@gmail.com>
2024-04-15 15:51:18 -0600

    Upgrade solution to new style to support NET8. Also add 2025 to the mix.

---
+++ src2/Methods/DynamoMethods.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.UI;
+using Dynamo.Applications;
+using Relay.Utilities;
+
+namespace Relay.Methods
+{
+    internal static class DynamoMethods
+    {
+        internal static Result RunGraph(UIApplication app, string dynamoJournal)
+        {
+            //toggle the graph to automatic. this is required for running Dynamo UI-Les
+            DynamoUtils.SetToAutomatic(dynamoJournal);
+
+            DynamoRevit dynamoRevit = new DynamoRevit();
+
+            IDictionary<string, string> journalData = new Dictionary<string, string>
+            {
+                {JournalKeys.ShowUiKey, false.ToString()},
+                {JournalKeys.AutomationModeKey, true.ToString()},
+                {JournalKeys.DynPathKey, ""},
+                {JournalKeys.DynPathExecuteKey, true.ToString()},
+                {JournalKeys.ForceManualRunKey, false.ToString()},
+                {JournalKeys.ModelShutDownKey, true.ToString()},
+                {JournalKeys.ModelNodesInfo, false.ToString()},
+            };
+            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData
+            {
+                Application = app,
+                JournalData = journalData
+            };
+
+            var result = dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
+
+//sorry folks, parks closed, the moose out front should have told you
+#if Revit2021Pro || Revit2022Pro || Revit2023Pro
+            Packages.ResolvePackages(DynamoRevit.RevitDynamoModel.PathManager.DefaultPackagesDirectory, dynamoJournal);
+#endif
+
+            DynamoRevit.RevitDynamoModel.OpenFileFromPath(dynamoJournal, true);
+            DynamoRevit.RevitDynamoModel.ForceRun();
+
+            return result;
+        }
+    }
+}
